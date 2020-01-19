0bc1753644bcf3eb3449739900bfe904f9c4714c
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2020-01-20 04:59:47 +0900

    Fix

--- service-bar/main.go
+++ service-bar/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "This is Bar Service!")
+		fmt.Fprintln(w, "This is Bar Service, Ver. 3!")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
--- service-foo/app.rb
+++ service-foo/app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  "This is Foo Service!!\n"
+  "This is Foo Service, Ver. 3!\n"
 end
