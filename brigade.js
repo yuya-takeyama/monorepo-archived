297bca4160e7f27e1de9213d820b4cce7327e16a
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2020-01-20 00:22:34 +0900

    kaniko

--- brigade.js
+++ brigade.js
@@ -1,6 +1,17 @@
 const { events, Job } = require('brigadier');
 
 events.on('push', (e, p) => {
-  console.log('e = %j', e);
-  console.log('p = %j', p);
+  const imageBuilder = new Job('image-builder');
+
+  imageBuilder.image = 'gcr.io/kaniko-project/executor';
+
+  imageBuilder.tasks = [
+    'cd /src',
+    'ls -lah',
+    'executor --help',
+  ];
+
+  imageBuilder.streamLogs = true;
+
+  imageRunner.run();
 });
