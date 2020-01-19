7b9dd7cb5b88c96f9f2f6192f58f7d49cba03ea1
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2020-01-20 02:03:01 +0900

    FIx

--- brigade.js
+++ brigade.js
@@ -30,7 +30,8 @@ events.on('push', async (e, project) => {
     'find . -name Dockerfile | awk -F / \'{ print $2 }\'',
   ];
 
-  const buildTargets = await buildDetector.run();
+  const buildDetectorResult = await buildDetector.run();
+  const buildTargets = buildDetectorResult.data.split('\n');
   console.log('buildTargets = %j', buildTargets);
 
   const imageBuilder = new Job('image-builder');
