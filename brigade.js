5e662805332bb403f24d52afcf77b6eeb19a5fe5
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2020-01-20 01:19:37 +0900

    Credential

--- brigade.js
+++ brigade.js
@@ -1,6 +1,38 @@
 const { events, Job } = require('brigadier');
 
-events.on('push', (e, p) => {
+events.on('push', async (e, project) => {
+  const kanikoCredentialLoader = new Job('kaniko-credential-loader');
+
+  kanikoCredentialLoader.image = 'alpine';
+
+  kanikoCredentialLoader.volumes = [
+    {
+      "name": "kaniko-credential-storage",
+      "emptyDir": {},
+    }
+  ];
+  kanikoCredentialLoader.volumeMounts = [
+    {
+      name: "kaniko-credential-storage",
+      mountPath: "/kaniko/.docker"
+    }
+  ];
+
+  const auth = {
+    auths: {
+      'https://index.docker.io/v1/': {
+        'auth': project.secrets.DOCKER_CREDENTIAL,
+      },
+    },
+  };
+  kanikoCredentialLoader.tasks = [
+    `echo '${JSON.stringify(auth)}' > /kaniko/.docker/config.json`,
+  ];
+
+  kanikoCredentialLoader.streamLogs = true;
+
+  await kanikoCredentialLoader.run();
+
   const imageBuilder = new Job('image-builder');
 
   imageBuilder.image = 'gcr.io/kaniko-project/executor';
@@ -12,7 +44,20 @@ events.on('push', (e, p) => {
     '--destination=yuyat/service-foo',
   ];
 
+  imageBuilder.volumes = [
+    {
+      "name": "kaniko-credential-storage",
+      "emptyDir": {},
+    }
+  ];
+  imageBuilder.volumeMounts = [
+    {
+      "name": "kaniko-credential-storage",
+      "mountPath": "/kaniko/.docker"
+    }
+  ];
+
   imageBuilder.streamLogs = true;
 
-  imageBuilder.run();
+  await imageBuilder.run();
 });
