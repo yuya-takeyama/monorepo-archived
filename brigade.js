89cf5acb3a270eef3923951fb5e372d15eec7181
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2020-01-20 04:30:34 +0900

    fix

--- brigade.js
+++ brigade.js
@@ -80,7 +80,7 @@ events.on('push', async (e, project) => {
 
       reorganizer.tasks = [
         `echo "${project.secrets.GIT_DEPLOY_KEY_BASE64}" > /tmp/id_rsa_base64`,
-        'cat /tm/id_rsa_base64 | base64 -D > ~/.ssh/id_rsa',
+        'cat /tmp/id_rsa_base64 | base64 -D > ~/.ssh/id_rsa',
         'chmod 400 ~/.ssh/id_rsa',
         'apk add --update bash git curl',
         'mkdir /kustomize',
