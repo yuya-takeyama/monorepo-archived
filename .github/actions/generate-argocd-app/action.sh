a5be8f248996c51a6b680e6550a10b0ba48582b2
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2021-03-27 03:00:49 +0900

    Separate actions

--- .github/actions/generate-argocd-app/action.sh
+++ .github/actions/generate-argocd-app/action.sh
@@ -81,33 +81,3 @@ spec:
       selfHeal: true
 EOS
 fi
-
-cd "${gitops_repo_dir}"
-
-if [ -z $(git status --porcelain) ]; then
-  echo "Nothing to commit"
-  exit
-fi
-
-branch="${service_name}/$(uuidgen)"
-git checkout -b "$branch"
-git config --global user.email "${GIT_USER_EMAIL}"
-git config --global user.name "${GIT_USER_NAME}"
-git add --all
-git commit -m "Update ${service_name}"
-git push -f origin "$branch"
-
-pull_request_body=""
-if [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
-	pr_number="${GITHUB_REF#"refs/pull/"}"
-	pr_number="${pr_number%"/merge"}"
-	pull_request_body="From https://github.com/${GITHUB_REPOSITORY}/pull/${pr_number}"
-else
-	origin_branch="${GITHUB_REF#"refs/heads/"}"
-	pull_request_body="From ${origin_branch} branch"
-fi
-
-gh pr create \
-	--title "Update ${service_name} in ${NAMESPACE}" \
-	--body "${pull_request_body}"
-gh pr merge --merge
