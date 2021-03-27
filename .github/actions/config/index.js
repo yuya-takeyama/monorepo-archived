9fd7cacc0b1f2d07a3fe9787695b7f487740e13f
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2021-03-27 18:54:59 +0900

    Fix

--- .github/actions/config/index.js
+++ .github/actions/config/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
+const path = require('path');
 const YAML = require('yaml');
 
-const file = fs.readFileSync('./config.yaml', 'utf8');
+const file = fs.readFileSync(path.join(__dirname, 'config.yaml'), 'utf8');
 config = YAML.parse(file);
 
 console.log(JSON.stringify(config, null, 2));
