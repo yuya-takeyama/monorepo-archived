acb20ffb145db968028d68dddbe78e87792b88fa
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2021-03-27 20:27:36 +0900

    FIx

--- .github/actions/config/index.js
+++ .github/actions/config/index.js
@@ -5,4 +5,4 @@ const YAML = require('yaml');
 const file = fs.readFileSync(path.join(__dirname, 'config.yaml'), 'utf8');
 config = YAML.parse(file);
 
-console.log(JSON.stringify(config, null, 2));
+console.log(`::set-output name=config::${JSON.stringify(config)}`);
